70d685477f29e4a2ee85a7d19afdf62ac9d88b57
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2025-01-13 21:13:35 +0500

    Created an App

--- myProject/views.py
+++ myProject/views.py
@@ -1,5 +1,5 @@
+from django.http import JsonResponse
 from django.http import HttpResponse
-
 from django.shortcuts import render
 
 # views.py
@@ -13,13 +13,17 @@ def about(request):
 
 def contact(request):
     from django.http import HttpResponse
+    
 
 def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
-        # Process the data (e.g., save to database, send an email, etc.)
-        return HttpResponse(f"Thank you, {name}. We have received your message.")
+
+        # You can save the form data here if needed
+
+        return JsonResponse({'success': True, 'message': f'Thank you, {name}. We have received your message.'})
+
     return render(request, 'website/contact.html')
 
